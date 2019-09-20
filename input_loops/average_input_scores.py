1c23ef694c30a183406010b3fb7df4ee896f535d
arickels11 <arickels93@gmail.com>
2019-09-20 15:07:05 -0500

    Initial Commit

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -2,9 +2,18 @@
 Alex Rickels
 Mod 5 Topic 2 - while loop """
 
+# declare a variable sum to store the scores total
+# declare a variable to store the number of items in the list, use len()
+# write a for loop to sum the items in the list
+# declare a variable avg to calculate the average
+# return  # variable avg
+
 
 def average(score_list):
-    pass
+    total_scores = sum(score_list)
+    items_in_list = len(score_list)
+    average_score = total_scores/items_in_list
+    return average_score
 
 
 if __name__ == '__main__':
